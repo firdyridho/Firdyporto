82e7b32569526b8568d7c0a13500144235f87df0
firdyridho <firdyridho9@gmail.com>
2025-12-11 10:22:26 +0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio/",
+  base: process.env.VITE_BASEE_PATH || "/Firdyporto",
   plugins: [react(), tailwindcss()],
-})
+});
